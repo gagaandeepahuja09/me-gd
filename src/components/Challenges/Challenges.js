7d092a05e2f814abcd2729d3922b1a202db60bb8
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2018-07-21 23:39:54 +0530

    completed challenges section

--- src/components/Challenges/Challenges.js
+++ src/components/Challenges/Challenges.js
@@ -9,21 +9,22 @@ const challenges = (props) => {
 				<div className = "col-1-of-2--1">
 					<div className = "card">
 						<div className = "card__side card__side--front">
-							<div className = "card__picture card__picture--1">
-							&nbsp;
+							<div className = "card__picture">
+								<div className = "card__picture--1"></div>
 							</div>
-							<div className = "card__details">
+							<div className = "card__details card__details--1">
 								<ul>
 									<li>Current Rating : <span>1753</span></li>
 									<li>Current Stars : <span>3</span></li>
 									<li>Highest Rating : <span>1835</span></li>
-									<li>Current Stars : <span>4</span></li>
+									<li>Best Stars : <span>4</span></li>
 									<li>Best Global Rank in a Contest of <span>204</span></li>
 									<li><span>April Challenge 2018</span></li>
 								</ul>
 							</div>
 						</div>
 						<div className = "card__side card__side--back card__side--back--1">
+							<ul><li><a href = "https://codechef.com" target = "_blank">Visit</a></li><li><a href = "https://codechef.com/users/gagand33p"  target = "_blank">View Profile</a></li></ul>
 						</div>
 					</div>
 				</div>
@@ -31,23 +32,31 @@ const challenges = (props) => {
 				<div className = "col-1-of-2--1">
 					<div className = "card">
 						<div className = "card__side card__side--front">
-							<div className = "card__picture card__picture--1">
-							&nbsp;
+							<div className = "card__picture">
+								<div className = "card__picture--2"></div>
 							</div>
-							<div className = "card__details">
+							<div className = "card__details card__details--2">
 								<ul>
-									<li>Current Rating : <span>1753</span></li>
-									<li>Current Stars : <span>3</span></li>
-									<li>Highest Rating : <span>1835</span></li>
-									<li>Current Stars : <span>4</span></li>
-									<li>Best Global Rank in a Contest of <span>204</span></li>
-									<li><span>April Challenge 2018</span></li>
+									<li>Current Rating : <span>1286</span></li>
+									<li>Current Position : <span>Pupil</span></li>
+									<li>Highest Rating : <span>1404</span></li>
+									<li>Best Position : <span>Specialist</span></li>
 								</ul>
 							</div>
 						</div>
-						<div className = "card__side card__side--back card__side--back--1">
+						<div className = "card__side card__side--back card__side--back--2">
+							<ul><li><a href = "https://codeforces.com" target = "_blank">Visit</a></li><li><a href = "https://codeforces.com/profile/Gagandeep98" target = "_blank">View Profile</a></li></ul>
 						</div>
 					</div>
+				</div>
+				<div className = "links">
+					<h3 className = "heading__tertiary">Other Profiles</h3>
+					<ul className = "others">
+						<a href = "https://www.hackerrank.com/gagandeep2598" target = "_blank"><li className = "hr"></li></a>
+						<a href = "https://www.interviewbit.com/profile/gagaandeepahuja09" target = "_blank"><li className = "ib"></li></a>
+						<a href = "https://leetcode.com/garyahuja/" target = "_blank"><li className = "lc"></li></a>
+						<a href = "https://www.hackerearth.com/@ahuja.gary" target = "_blank"><li className = "he"></li></a>
+					</ul>
 				</div>
 			</div>
 		</div>
